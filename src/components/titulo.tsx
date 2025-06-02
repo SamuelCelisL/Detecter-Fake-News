5a5c284589561d405cf2903c78424fac096de4f1
SamuelCelisL <samuelcelislizcano@gmail.com>
2025-06-02 00:27:28 -0500

    Implement code changes to enhance functionality and improve performance

--- src/components/titulo.tsx
+++ src/components/titulo.tsx
@@ -1,8 +1,13 @@
 export default function Titulo() {    
     return (
-        <h1 className="text-3xl font-bold text-center m-4 border">
+    < nav className="flex justify-center py-5">
+        <div className="flex justify-center items-center  rounded-2xl w-[700px] bg-gradient-to-r from-blue-500 to-green-500 p-4 shadow-lg hover:shadow-2xl  hover:shadow-white hover:text-white">
+            <img src="/logo.png" alt="Logotipo de la aplicacion" className="size-15 " />
+            <h1 className="text-3xl font-bold text-center font-Georgia ">
             Detecter Fake News
-        </h1>
+            </h1>
+        </div>
+    </nav>
     );
   
 }
