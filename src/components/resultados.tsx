62bdfd55437cc1d85a0072dbbe462fc402fe7aae
SamuelCelisL <samuelcelislizcano@gmail.com>
2025-06-09 15:35:45 -0500

    Eliminar código comentado y limpiar el componente Resultados para mejorar la legibilidad.

--- src/components/resultados.tsx
+++ src/components/resultados.tsx
@@ -1,9 +1,7 @@
 export default function Resultados() {
-    // conts {texto}="algo nose ajjajajja";
     return (
         <div className="flex flex-col flex-wrap w-auto items-center justify-center p-4 shadow-lg rounded-2xl bg-gradient-to-l from-blue-500 to-green-500 hover:shadow-2xl hover:shadow-white hover:text-white hover:border m-6">
             <h1 className="text-2xl font-bold mb-4 text-black">Resultados</h1>
-            {/* <p className="text-lg text-black mb-2 uppercase underline">Aquí se mostrarán los resultados.</p> */}
             <table className="table-auto border-b border-gray-500 max-w-[600px] w-auto md:w-auto lg:w-auto text-black bg-transparent">
                 <thead>
                     <tr>
