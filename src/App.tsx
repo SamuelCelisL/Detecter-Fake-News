4d1af3b0df6389ef911df2931e212fc510c2acad
SamuelCelisL <samuelcelislizcano@gmail.com>
2025-06-11 23:05:40 -0500

    Ajustar estructura y estilos en componentes para mejorar la presentación y responsividad

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   const [error, setError] = useState<string | null>(null);
 
   return (
-    <>
+    <div className="flex flex-col items-center justify-center gap-4">
       <Titulo />
       <Informacion />
       <Enlaces setResultado={setResultado} setError={setError}/>
@@ -18,7 +18,7 @@ function App() {
       <p className="text-black">
         Desarrollado por <a className='font-bold text-blue-900 hover:text-white underline' href="https://www.linkedin.com/in/samuel-celis-l/" target='_blank'>Samuel Andres Celis Lizcano</a>
       </p>
-    </>
+    </div>
   )
 }
 
